e66a118453bcd22ee1ecf09e5d50af8b1eb93561
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-03-27 00:00:15 +0530

    create Footer component UI

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="flex flex-col items-center bg-zinc-50 text-center text-surface dark:bg-neutral-700 dark:text-white lg:text-left">
+      <div class="w-full bg-black/5 p-4 text-center">
+        © 2024 Copyright:
+        <a href="/">Prasad Madhuranga</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
