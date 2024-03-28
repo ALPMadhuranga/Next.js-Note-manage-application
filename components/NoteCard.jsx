d86521fa916ba060575ed659460fe94a9191ccef
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-03-28 17:38:21 +0530

    note created time and date view on note card

--- components/NoteCard.jsx
+++ components/NoteCard.jsx
@@ -1,36 +1,82 @@
 import Link from "next/link";
-import React from "react";
-import { FaTrash, FaEdit, FaEye } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { FaAngleRight, FaTrash, FaEdit, FaEye } from "react-icons/fa";
 
-const NoteCard = () => {
+const NoteCard = ({ session }) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const response = await fetch(`/api/user/${session?.user.id}/note`);
+      const data = await response.json();
+
+      setNotes(data);
+    };
+
+    if (session?.user.id) {
+      fetchNotes();
+    }
+  }, []);
+
+  const formatCreatedAt = (createdAt) => {
+    const date = new Date(createdAt);
+    const formattedDate = `${date.getDate().toString().padStart(2, '0')}:${(date.getMonth() + 1).toString().padStart(2, '0')}:${date.getFullYear()} | ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+    return formattedDate;
+  };
+
   return (
-    <div className="bg-white shadow-lg rounded-md p-6 gap-4 w-full md:w-1/2 lg:w-1/3 mb-6">
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold mb-4 border-b border-gray-300">
-          Title
-        </h2>
-        <p className="text-gray-700 mb-4 text-sm leading-6 mt-4 py-2 border-b">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          quaerat quae odio tenetur expedita soluta vero non quis, blanditiis
-          maiores dolore reiciendis cupiditate qui dolorum! Ipsa qui quam aut
-          voluptate.
-        </p>
-      </div>
-      <div className="flex justify-end space-x-4 text-xl">
-        <button className="text-red-500 hover:text-red-700" aria-label="Delete">
-          <FaTrash />
-        </button>
-        <Link href="/updateNote" className="text-blue-500 hover:text-blue-700" aria-label="Edit">
-          <FaEdit />
-        </Link>
-        <button
-          className="text-teal-500 hover:text-teal-700"
-          aria-label="View"
-        >
-          <FaEye />
-        </button>
-      </div>
-    </div>
+    <>
+      {notes && notes.length > 0 ? (
+        notes.map((note, index) => (
+          <div className="bg-white shadow-lg rounded-md p-6 gap-4 w-full h-80 md:w-1/2 lg:w-1/3 mb-6 flex flex-col">
+            <div className=" transition-all duration-300 transform">
+              <h2 className="text-xl font-semibold border-gray-300 overflow-auto flex flex-row">
+                <FaAngleRight className="text-teal-500 text-2xl mr-2" />
+                {note.title}
+              </h2>
+            </div>
+
+            <div className="mb-2 flex-grow overflow-auto border-b border-t">
+              <p className="text-gray-700 mb-4 text-sm leading-6 py-2 text-start">
+                {note.description}
+              </p>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="text-sm " >
+                <p>
+                  {formatCreatedAt(note.createdAt)}
+                </p>
+              </div>
+              <div className="flex space-x-4 text-xl" >
+              <button
+                className="text-red-500 hover:text-red-700"
+                aria-label="Delete"
+              >
+                <FaTrash />
+              </button>
+              <Link
+                href="/updateNote"
+                className="text-blue-500 hover:text-blue-700"
+                aria-label="Edit"
+              >
+                <FaEdit />
+              </Link>
+              <button
+                className="text-teal-500 hover:text-teal-700"
+                aria-label="View"
+              >
+                <FaEye />
+              </button>
+              </div>
+             
+            </div>
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
