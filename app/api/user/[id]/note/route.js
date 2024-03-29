45a900e0092c7c60778a119359831e11bba50e23
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-03-29 09:32:41 +0530

    update get functionality

--- app/api/user/[id]/note/route.js
+++ app/api/user/[id]/note/route.js
@@ -7,7 +7,7 @@ export const GET = async (request, { params }) => {
     try {
       await connect();
   
-      const notes = await Note.find({ author: params.id });
+      const notes = await Note.find({ author: params.id }).short({ createdAt: -1 });
   
       return new NextResponse(JSON.stringify(notes), { status: 200 });
     } catch (error) {
